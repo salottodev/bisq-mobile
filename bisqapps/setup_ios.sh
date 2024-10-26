47c3a1a36653348d9ab7320d7344dba3eeb35212
rodvar <1040902+rodvar@users.noreply.github.com>
2024-10-26 15:20:51 +1100

    - script to facilitate ios project pod update

---
+++ bisqapps/setup_ios.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run from the project root
+echo "Installing Cocoapods dependencies..."
+cd iosClient || exit
+pod install
+
+echo "Building Kotlin/Native frameworks..."
+cd .. || exit
+./gradlew :shared:assembleXCFramework
+./gradlew :sharedUI:assembleXCFramework
+# ./gradlew :shared:presenter:assembleXCFramework
+# ./gradlew :shared:utilities:assembleXCFramework
+
+echo "Opening Xcode workspace..."
+open iosClient/iosClient.xcworkspace
