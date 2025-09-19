ef3b9716cb2a4eb7ea72a85d2202df115a8ef777
HenrikJannsen <boilingfrog@gmx.com>
2025-09-19 22:54:17 +0700

    Add more fields for state about network connectivity and inventory requests

--- shared/domain/src/commonMain/kotlin/network/bisq/mobile/domain/service/network/NetworkServiceFacade.kt
+++ shared/domain/src/commonMain/kotlin/network/bisq/mobile/domain/service/network/NetworkServiceFacade.kt
@@ -5,5 +5,10 @@ import network.bisq.mobile.domain.LifeCycleAware
 import network.bisq.mobile.domain.utils.Logging
 
 interface NetworkServiceFacade : LifeCycleAware, Logging {
+    val minConnections: Int get() = 3
     val numConnections: StateFlow<Int>
+    val targetNumConnectedPeers: StateFlow<Int>
+    val numPendingRequests: StateFlow<Int>
+    val maxPendingRequestsAtStartup: StateFlow<Int>
+    val allDataReceived: StateFlow<Boolean>
 }
